803a4850f10e8d45424ae8bba36606d3638476d9
Guilherme <Guilherme@WTTPC90.wtt.local>
2019-02-06 13:18:03 -0200

    atualização remove-registro.js

--- js/remove-registro.js
+++ js/remove-registro.js
@@ -1,4 +1,19 @@
-var botaoExcluir = document.querySelector(".paginalistagem-botao-excluir");
+
+var botaoExcluir = document.querySelectorAll(".paginalistagem-botao-excluir");
+
+for (const button of botaoExcluir) {
+ 
+button.addEventListener("click", function(event) {
+    event.target.parentNode.parentNode.classList.add("fadeOut");
+});
+}
+
+setTimeout(function() {
+event.target.parentNode.parentNode.remove();
+}, 500);
+
+
+/*var botaoExcluir = document.querySelector(".paginalistagem-botao-excluir");
 
 botaoExcluir.addEventListener("click", function(event) {
     event.target.parentNode.parentNode.classList.add("fadeOut");
@@ -6,5 +21,4 @@ botaoExcluir.addEventListener("click", function(event) {
 
 setTimeout(function() {
     event.target.parentNode.parentNode.remove();
-}, 500);
-
+}, 500);*/
